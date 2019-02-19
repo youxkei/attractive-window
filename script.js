6afd85ab96456de3bd753a3295412bb2f2311015
youxkei <youxkei@gmail.com>
2019-02-19 14:16:47 +0900

    Ignore pinned tabs

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 chrome.tabs.onCreated.addListener(tab => {
+  if (tab.pinned) {
+    return
+  }
+
   chrome.windows.getAll({ populate: true }, windows => {
     const specifiedWindows = windows.filter(window => window.tabs[0].url === "about:blank")
     if (specifiedWindows.length === 0) {
